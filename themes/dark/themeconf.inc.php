b1c1d1f04df2c4a8909e69d495f22cfaf42e6546
Peter Gnodde (wanda.home.gnodde.org) <peter@gnodde.org>
2018-05-08 22:40:16 +0200

    update all themes

--- themes/dark/themeconf.inc.php
+++ themes/dark/themeconf.inc.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Theme Name: dark
-Version: auto
+Version: 2.9.3
 Description: Dark background, grayscale.
 Theme URI: http://piwigo.org/ext/extension_view.php?eid=369
 Author: Piwigo team
--- themes/smartpocket/themeconf.inc.php
+++ themes/smartpocket/themeconf.inc.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Theme Name: Smart Pocket
-Version: auto
+Version: 2.9.3
 Description: Mobile theme.
 Theme URI: http://piwigo.org/ext/extension_view.php?eid=599
 Author: P@t
